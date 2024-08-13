8e99efeb8bb74101f888b99756aad7f11de164d4
Subhanshu Chaddha <subhanshu.chaddha@tagbin.in>
2024-08-13 19:16:04 +0530

    Launchong soon initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="launching-soon-container">
+      <h1>Launching Soon</h1>
+      <p>We are working hard to bring something amazing. Stay tuned!</p>
     </div>
   );
 }
